3fb6d8f9d2bb5b6aa60a8fff896845980e8c7bb9
rodanAsh <aakashash2002@gmail.com>
2024-12-18 20:21:15 +0530

    feat - worked on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,21 @@ const Hero = () => {
             <div className="container">
                 {/* Navabar section */}
                 {/* Hero section */}
-                <div className='grid grid-cols-1 md:grid-cols-2'></div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]'>
+                    {/* Text Content Section */}
+                    <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
+                        <h1 className='text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
+                        <div className='relative'>
+                            <div className='relative z-10 space-y-4'>
+                                <h1 className='text-2xl'>Black Lifestyle Lovers,</h1>
+                                <h1 className='text-sm opacity-55 leading-loose'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus aspernatur, cumque eos neque dolorem architecto deserunt quis voluptatibus in quisquam quia ducimus</h1>
+                            </div>
+                        </div>
+                        <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25'></div>
+                    </div>
+                    {/* Hero Image Section */}
+                    {/* Third div section */}
+                </div>
             </div>
         </section>
     </main>
