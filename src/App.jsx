1bf73c53240e3fc49b796da40aae79105c5143ab
rodanAsh <aakashash2002@gmail.com>
2025-01-02 11:02:12 +0530

    feat: designed appbanner component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import WhereToBuy from './components/WhereToBuy'
+import AppBanner from './components/AppBanner'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Services />
       <WhereToBuy />
+      <AppBanner />
     </main>
   )
 }
